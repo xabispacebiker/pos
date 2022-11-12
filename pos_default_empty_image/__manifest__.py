9fb65bf4095f852d71e540a5826950047e9c2519
Odoo Masters <info@odoomasters.com>
2022-11-12 12:04:21 +0100

    [FIX] pos_default_empty_image

--- pos_default_empty_image/__manifest__.py
+++ pos_default_empty_image/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # © 2015 Akretion, GRAP, OCA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
--- pos_default_empty_image/models/__init__.py
+++ pos_default_empty_image/models/__init__.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
+
 from . import pos_config
+
